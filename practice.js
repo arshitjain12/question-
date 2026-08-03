63a7ee0f7dde36c797220fc08f26d3d217157780
Arshit Jain <arshit.j@zecdata.com>
2026-08-03 15:35:33 +0530

    add number

--- practice.js
+++ practice.js
@@ -1,19 +1,13 @@
-// find min number
+// add  number
 let arr = [5,6,9,2,8];
-function min(arr){
-         let min = arr[0] 
+ function sum(arr){
+    let sum = 0;
     for (let i = 0; i < arr.length; i++) {
-
-        if (arr[i]>min) {
-            min = arr[i]
-        }
+             sum +=arr[i]
+        
     }
-    return min
-     
-}
-  console.log(min(arr));
-//   ...
-let arr = [5,6,9,2,8];
- let max = Math.max(...arr)
- console.log(max);
+    return sum 
+ }
+ console.log(sum (arr));
+ 
  

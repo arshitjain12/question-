28d8a251d192db27314150cd7583080e39d4e6dd
Arshit Jain <arshit.j@zecdata.com>
2026-08-03 16:45:28 +0530

    rotaion by k

--- practice.js
+++ practice.js
@@ -1,8 +1,10 @@
-// question 10 miss num 
-let arr = [1,2,3,5];
-let n = arr.length+1;
-let totalSum = (n*(n+1))/2;
-let arrReducer = arr.reduce((acc , val)=>acc+val,0)
-let value  = totalSum-arrReducer;
-console.log(value);
-                                                
+	// rotation by k value 
+    let arr = [1,2,3,5,9,7,6];
+	let rotation = (arr,k)=>{
+     k = k%arr.length;
+    for (let i = 0; i <k; i++) {
+       let last = arr.pop();
+       arr.unshift(last);        
+    } return arr;
+} 
+console.log(rotation(arr,3))

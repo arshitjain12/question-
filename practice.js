4c3fc09a3d33bde3fdd02cbe374250c3062ef7c7
Arshit Jain <arshit.j@zecdata.com>
2026-08-05 18:24:57 +0530

    intersection

--- practice.js
+++ practice.js
@@ -1,17 +1,7 @@
-//  question 15 find all zero 
- function zero(arr){
-    let poss = 0;
-    for (let i = 0; i < arr.length; i++) {
-        if(arr[i]!==0){
-            arr[poss] =arr[i]
-            poss++
-        };
-        
-    }
-    while (poss<arr.length){
-            arr[poss] = 0
-            poss++
-    }
-    return arr
- }
-  console.log(zero([1,2,0,6,0,4,0,2,0]))
+// intersection 
+const arr1 = [1, 2, 3, 4];
+const arr2 = [3, 4, 5, 6];
+
+const intersection = arr1.filter(value => arr2.includes(value));
+
+console.log(intersection);
